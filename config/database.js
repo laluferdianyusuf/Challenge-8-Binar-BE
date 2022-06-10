c8759a2f35d9c906eba4079fd7ee03b00aa475de
Ferdian Yusuf <lferdianyusuf@gmail.com>
2022-06-11 00:02:42 +0800

    Initial Commit

--- config/database.js
+++ config/database.js
@@ -26,7 +26,7 @@ module.exports = {
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_test`,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
@@ -40,7 +40,7 @@ module.exports = {
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_production`,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
